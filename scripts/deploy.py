c20dfb06b0c90f92b49189a55a3bbdfba0d98995
Entreprenerd <valeranijr@gmail.com>
2021-08-11 16:56:29 +0200

    feat: deploy script

---
+++ scripts/deploy.py
@@ -0,0 +1,37 @@
+"""
+    Deploy the Strategy Logic and the Proxy
+"""
+from scripts.helpers.connect_account import connect_account
+import click
+from brownie import AdminUpgradeabilityProxy, config, web3, project
+
+## Load ContractContainers from Badger Vaults
+
+proxyAdmin = web3.toChecksumAddress("0xB10b3Af646Afadd9C62D663dd5d226B15C25CdFA"),
+
+
+def deploy_registry_logic(logic):
+    """
+    Deploy the strat logic
+    """
+    dev = connect_account()
+
+    if click.confirm("Deploy New Registry"):
+        args = []
+
+        strat_logic = logic.deploy({'from': dev})
+        registry_proxy = AdminUpgradeabilityProxy.deploy(strat_logic, proxyAdmin, strat_logic.initialize.encode_input(*args), {'from': dev})
+        
+        ## We delete from deploy and then fetch again so we can interact
+        AdminUpgradeabilityProxy.remove(registry_proxy)
+        registry_proxy = logic.at(registry_proxy.address)
+
+        print(registry_proxy)
+        print(dir(registry_proxy))
+        click.echo(f"New Registry Release deployed [{registry_proxy.address}]")
+
+        return registry_proxy
+
+def main():
+    strat = deploy_registry_logic(AdminUpgradeabilityProxy)
+    return strat
